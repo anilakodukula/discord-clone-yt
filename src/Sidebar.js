22054ccddd3ca2958f9b33ea858d91a572a363b6
anilakodukula <anilakodukula@gmail.com>
2020-12-15 22:09:40 -0500

    built out the sidebar

---
+++ src/Sidebar.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import "./Sidebar.css";
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import AddIcon from '@material-ui/icons/Add';
+import SidebarChannel from './SidebarChannel';
+import SignalCellularAltIcon from '@material-ui/icons/SignalCellularAlt';
+import CallIcon from '@material-ui/icons/Call';
+import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined';
+
+
+function Sidebar() {
+    return (
+        <div className = "Sidebar">
+         
+
+                <div className="sidebar__top">
+                    <h3>Anila Kodukula</h3>
+                    <ExpandMoreIcon />
+                </div>
+            <div className="sidebar__channels"> 
+                <div className="sidebar__channelsHeader">
+                    <div className="sidebar__header">
+                        <ExpandMoreIcon />
+                        <h4>Text Channels</h4>
+                    </div>
+                    <AddIcon className= "sidebar__addChannel" /> 
+                </div>
+                <div className="sidebar__channelsList">
+                <SidebarChannel />
+                <SidebarChannel />
+                <SidebarChannel />
+                <SidebarChannel />
+                <SidebarChannel />
+                </div>
+            </div>
+            <div className="sidebar__voice">
+                <SignalCellularAltIcon className= "sidebar__voiceIcon"
+                fontSize = "large" />
+                <div className="sidebar__voiceInfo">
+                    <h3>Voice Connected</h3>
+                    <p>Stream</p>
+                    
+                </div>
+                <div className="sidebar__voiceIcons">
+                       <InfoOutlinedIcon />
+                       <CallIcon />
+
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar
